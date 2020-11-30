fee789df75cc67eafcd413af1cf309e60c42b151
coliver0221 <coliver0221.cs03@nctu.edu.tw>
2020-11-30 14:29:33 +0800

    feat: complete home page graph
    Complete home page graph for 1920*1080

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,11 +1,61 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import styled from '@emotion/styled';
+
+// constant
+import { graphScale } from '../../constants';
+import courseNodes from '../../constants/courseNodes';
+// assets
+import courseMap from '../../static/BigDataRoadMap.svg';
 
 function Home() {
+  const history = useHistory();
   return (
-    <div>
-      <p>Hello Home Page</p>
-    </div>
+    <HomePageContainer scale={graphScale}>
+      <MapContainer src={courseMap}>
+        {courseNodes.map((course) => (
+          <CoursePoint
+            key={course.id}
+            top={course.top}
+            left={course.left}
+            color={course.color}
+            onClick={() => { history.push(`/course/${course.id}`); }}
+          />
+        ))}
+      </MapContainer>
+    </HomePageContainer>
   );
 }
 
 export default Home;
+
+const HomePageContainer = styled.div`
+  position: relative;
+  margin: 0 auto;
+  width: calc(3975px * ${(props) => props.scale});
+  height: calc(1513px * ${(props) => props.scale});
+  padding: 5px;
+`;
+
+const MapContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background-image: url(${(props) => props.src});
+  background-size: contain;
+  background-repeat: no-repeat;
+`;
+
+const CoursePoint = styled.div`
+  position: absolute;
+  top: ${(props) => props.top}px;
+  left: ${(props) => props.left}px;
+  width: calc(60px * 0.45);
+  height: calc(60px * 0.45);
+  border-radius: 100%;
+  cursor: pointer;
+
+  :hover {
+    background-color: ${(props) => props.color};
+  }
+`;
---
+++ src/constants/courseNodes.jsx
@@ -0,0 +1,174 @@
+import { graphScale } from './index';
+
+const courseNodes = [
+  {
+    id: 'storageProcess',
+    top: 285 * graphScale,
+    left: 843 * graphScale,
+    color: ' #67ab9f',
+  },
+  {
+    id: 'storageAnalysis',
+    top: 285 * graphScale,
+    left: 1121 * graphScale,
+    color: ' #67ab9f',
+  },
+  {
+    id: 'dataCollection',
+    top: 728 * graphScale,
+    left: 741 * graphScale,
+    color: ' #ff9354',
+  },
+  {
+    id: 'dataEvaluation',
+    top: 728 * graphScale,
+    left: 914 * graphScale,
+    color: ' #ff9354',
+  },
+  {
+    id: 'featureSelection',
+    top: 726 * graphScale,
+    left: 1103 * graphScale,
+    color: ' #ff9354',
+  },
+  {
+    id: 'DimensionReduction',
+    top: 725 * graphScale,
+    left: 1274 * graphScale,
+    color: ' #ff9354',
+  },
+  {
+    id: 'probabilityTheory',
+    top: 1129 * graphScale,
+    left: 781 * graphScale,
+    color: ' #003366',
+  },
+  {
+    id: 'variationAnalysis',
+    top: 1126 * graphScale,
+    left: 1039 * graphScale,
+    color: ' #003366',
+  },
+  {
+    id: 'correlationCoefficient',
+    top: 1128 * graphScale,
+    left: 1274 * graphScale,
+    color: ' #003366',
+  },
+  {
+    id: 'distributedSystem',
+    top: 322 * graphScale,
+    left: 1734 * graphScale,
+    color: ' #5c91e0',
+  },
+  {
+    id: 'distributedStorage',
+    top: 323 * graphScale,
+    left: 1894 * graphScale,
+    color: ' #5c91e0',
+  },
+  {
+    id: 'distributedCoculate',
+    top: 321 * graphScale,
+    left: 2051 * graphScale,
+    color: ' #5c91e0',
+  },
+  {
+    id: 'distributedManager',
+    top: 318 * graphScale,
+    left: 2223 * graphScale,
+    color: ' #5c91e0',
+  },
+  {
+    id: 'distributedStreaming',
+    top: 318 * graphScale,
+    left: 2371 * graphScale,
+    color: ' #5c91e0',
+  },
+  {
+    id: 'distributedCoordination',
+    top: 315 * graphScale,
+    left: 2522 * graphScale,
+    color: ' #5c91e0',
+  },
+  {
+    id: 'MLClassification',
+    top: 781 * graphScale,
+    left: 1752 * graphScale,
+    color: ' #ea6e6e',
+  },
+  {
+    id: 'MLProcess',
+    top: 783 * graphScale,
+    left: 1929 * graphScale,
+    color: ' #ea6e6e',
+  },
+  {
+    id: 'MLClassificationAnalysis',
+    top: 782 * graphScale,
+    left: 2093 * graphScale,
+    color: ' #ea6e6e',
+  },
+  {
+    id: 'MLRegressionAnalysis',
+    top: 779 * graphScale,
+    left: 2279 * graphScale,
+    color: ' #ea6e6e',
+  },
+  {
+    id: 'MLClusterAnalysis',
+    top: 777 * graphScale,
+    left: 2442 * graphScale,
+    color: ' #ea6e6e',
+  },
+  {
+    id: 'supervisedLearning',
+    top: 628 * graphScale,
+    left: 3000 * graphScale,
+    color: ' #17544c',
+  },
+  {
+    id: 'unSupervisedLearning',
+    top: 633 * graphScale,
+    left: 3253 * graphScale,
+    color: ' #17544c',
+  },
+  {
+    id: 'model',
+    top: 1016 * graphScale,
+    left: 3002 * graphScale,
+    color: ' #7160b8',
+  },
+  {
+    id: 'modelFree',
+    top: 1018 * graphScale,
+    left: 3255 * graphScale,
+    color: ' #7160b8',
+  },
+  {
+    id: 'visualTool',
+    top: 1391 * graphScale,
+    left: 2871 * graphScale,
+    color: ' #ffdb87',
+  },
+  {
+    id: 'visualScience',
+    top: 1393 * graphScale,
+    left: 3047 * graphScale,
+    color: ' #ffdb87',
+  },
+  {
+    id: 'dataVisualization',
+    top: 1392 * graphScale,
+    left: 3221 * graphScale,
+    color: ' #ffdb87',
+  },
+  {
+    id: 'visualAnalysis',
+    top: 1388 * graphScale,
+    left: 3385 * graphScale,
+    color: ' #ffdb87',
+  },
+];
+
+export default courseNodes;
