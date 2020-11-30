6f0f2b9a7efdcc82059f33d26c90c1b4c35ae732
coliver0221 <coliver0221.cs03@nctu.edu.tw>
2020-11-30 13:56:33 +0800

    feat: import chakra.ui package

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
 // components
 import Home from './components/home';
+// theme
+import customTheme from './styles/customTheme';
 
 function App() {
+  const theme = extendTheme(customTheme);
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/" exact component={Home} />
-        </Switch>
-      </div>
-    </Router>
+    <ChakraProvider resetCSS theme={theme}>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route path="/" exact component={Home} />
+          </Switch>
+        </div>
+      </Router>
+    </ChakraProvider>
   );
 }
 
---
+++ src/styles/customTheme.jsx
@@ -0,0 +1,5 @@
+const customTheme = {
+  // define our own theme below
+};
+
+export default customTheme;
